66b40616aac15af192298e74675fa8692a21c8df
Yoav0akr <carlosyoav08@gmail.com>
2025-12-15 07:57:07 -0600

    Add files via upload

--- admins.js
+++ admins.js
@@ -4,7 +4,7 @@
 const adminpass = localStorage.getItem("adminpass") === "true";
 if (!adminpass) {
   alert("No tienes permisos para acceder a esta página.");
-  window.location.href = "./index.html";
+  //window.location.href = "./index.html";
 }
 
 // ===============================
